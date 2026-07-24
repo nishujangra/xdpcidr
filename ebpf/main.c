fde6bc53c9f8d695e8c2f62115e145328ecb6306
nishujangra <ndjangra1027@gmail.com>
2026-07-25 01:11:34 +0530

    feat: sanity checks

--- ebpf/main.c
+++ ebpf/main.c
@@ -1,13 +1,45 @@
-// Developed by: Nishnat ndjangra1027@gmail.com -- nishujangra.dev
+// Developed by: Nishant ndjangra1027@gmail.com -- nishujangra.dev
 
 #include <bpf/bpf_helpers.h>
 #include <linux/bpf.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
 
 // Main entry point for XDP
 SEC("xdp")
 int xdp_cidr(struct xdp_md *ctx) {
     void *data = (void *)(long)(ctx->data);
     void *data_end = (void *)(long)(ctx->data_end);
+
+    // sanity check eth header
+    struct ethhdr *eth = data;
+    if ((void *)(eth + 1) > data_end) {
+        return XDP_ABORTED;
+    }
+
+    // Check for IPv4
+    if (bpf_ntohs(eth->h_proto) == ETH_P_IP) {
+        struct iphdr *iph = (void *)(eth + 1);
+
+        // bound check
+        if ((void *)(iph + 1) > data_end) {
+            return XDP_ABORTED;
+        }
+
+        // ihl is in 32-bit words, 5 is the minimum (no options)
+        if (iph->ihl < 5) {
+            return XDP_ABORTED;
+        }
+
+        if ((void *)iph + iph->ihl * 4 > data_end) {
+            return XDP_ABORTED;
+        }
+    }
+
+    // Drop IPv6
+    else if (bpf_ntohs(eth->h_proto) == ETH_P_IPV6) {
+        return XDP_DROP;
+    }
 
     return XDP_PASS;
 }
