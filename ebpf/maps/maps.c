6a8b90968660b03ff44fffc6d8feb66eab2fae2e
nishujangra <ndjangra1027@gmail.com>
2026-07-25 01:22:08 +0530

    feat: blocklist map added

---
+++ ebpf/maps/maps.c
@@ -0,0 +1,15 @@
+// --- Developed By: Nishant ndjangra1027@gmail.com -- nishujangra.dev
+
+#include "struct.h"
+#include <bpf/bpf_helpers.h>
+#include <linux/bpf.h>
+
+#define MAX_BLOCKLIST 1000
+
+// Blacklist IPv4
+struct {
+    __uint(type, BPF_MAP_TYPE_PERCPU_HASH);
+    __type(key, struct ipv4_key);
+    __type(value, struct ip_meta);
+    __unit(max_entries, MAX_BLOCKLIST);
+} blk_ip_v4 SEC(".maps");
