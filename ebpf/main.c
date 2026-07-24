152fe0ba2976a060152b0019299954f39783dd4f
nishujangra <ndjangra1027@gmail.com>
2026-07-25 00:56:53 +0530

    fix: missing library imports

--- ebpf/main.c
+++ ebpf/main.c
@@ -1,7 +1,15 @@
 // Developed by: Nishnat ndjangra1027@gmail.com -- nishujangra.dev
 
+#include <bpf/bpf_helpers.h>
+#include <linux/bpf.h>
+
 // Main entry point for XDP
 SEC("xdp")
-int xdp_cidr(struct xdp_md *ctx) { return XDP_PASS; }
+int xdp_cidr(struct xdp_md *ctx) {
+    void *data = (void *)(long)(ctx->data);
+    void *data_end = (void *)(long)(ctx->data_end);
+
+    return XDP_PASS;
+}
 
 char LICENSE[] SEC("license") = "GPL";
