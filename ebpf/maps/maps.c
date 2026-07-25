95cdb49e0be39ef3c05f98c601066917cd195d9f
nishujangra <ndjangra1027@gmail.com>
2026-07-25 23:10:12 +0530

    feat: ebpf map for CIDR blocking

--- ebpf/maps/maps.c
+++ ebpf/maps/maps.c
@@ -13,3 +13,11 @@ struct {
     __type(value, struct ip_meta);
     __unit(max_entries, MAX_BLOCKLIST);
 } blk_ip_v4 SEC(".maps");
+
+// Blacklist IPv4 CIDR
+struct {
+    __uint(type, BPF_MAP_TYPE_LPM_TRIE);
+    __type(key, struct ipv4_lpm_key);
+    __type(value, struct ip_meta);
+    __uint(max_entries, MAX_BLOCKLIST);
+} blk_cidr_v4 SEC(".maps");
