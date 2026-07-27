580b61aaafb4a11d3cdce115010fc7aaf702b5df
nishujangra <ndjangra1027@gmail.com>
2026-07-28 00:31:54 +0530

    feat: cli arguments for interface and ebpf_path

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+// --- Developed By: Nishant ndjangra1027@gmail.com -- nishujangra.dev
+
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(about = "xdpcidr - eBPF based blocklist")]
+struct CliArgs {
+    #[arg(short, long, default_value = "eth0")]
+    interface: String,
+
+    #[arg(short, long, default_value = "/tmp/xdpcidr/xdpcidr.o")]
+    ebpf_path: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Ohh yeah!!!!!!!!!!!!!");
 }
