1d2a8ac20d45df5bf97ac444b41e6b142bb3c935
nishujangra <ndjangra1027@gmail.com>
2026-07-30 00:23:31 +0530

    feat: load ebpf code and detach using Ctrl+C

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 // --- Developed By: Nishant ndjangra1027@gmail.com -- nishujangra.dev
 
+use aya::programs::XdpFlags;
+use aya::{Ebpf, programs::Xdp};
 use clap::Parser;
 
 #[derive(Debug, Parser)]
@@ -12,6 +14,29 @@ struct CliArgs {
     ebpf_path: String,
 }
 
-fn main() {
+//#[tokio::main]
+fn main() -> Result<(), anyhow::Error> {
     println!("Ohh yeah!!!!!!!!!!!!!");
+
+    let args = CliArgs::parse();
+
+    let mut ebpf = Ebpf::load_file(&args.ebpf_path)?;
+
+    let program: &mut Xdp = ebpf
+        .program_mut("xdp_cidr")
+        .ok_or_else(|| anyhow::anyhow!("program xdpcidr not found in ELF"))?
+        .try_into()?;
+
+    program.load()?;
+    program.attach(&args.interface, XdpFlags::default())?;
+
+    println!("rfw-ebpf program attached. Press Ctrl+C to stop.");
+
+    let (tx, rx) = std::sync::mpsc::channel();
+    ctrlc::set_handler(move || { let _ = tx.send(()); })?;
+    rx.recv()?;
+
+    println!("\n\nDetaching...");
+
+    Ok(())
 }
